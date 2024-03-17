e918274d3f059936bb832df2af4ca4d54c66a36c
SHRADDHA SONAWANE <shraddhasonawane524@gmail.com>
2024-03-17 14:29:58 +0530

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+  return ( <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center'> 
+            <div className='pb-8'> 
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className=' text-xl '>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid possimus amet ducimus architecto quod atque laudantium eveniet, quae numquam, repellat hic ex libero sit odio excepturi vel. Repudiandae itaque a nesciunt quas harum nemo iste blanditiis, dolorum vero reprehenderit. Praesentium incidunt doloribus delectus accusamus, enim atque magnam reiciendis fuga repellat.
+            </p>
+            <br/>
+            <p className='text-xl'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo facilis suscipit voluptatem reiciendis sed esse voluptate hic ea numquam, unde, sequi repellendus consequatur labore molestiae dolore. Repellat amet aperiam, voluptatem tempore incidunt mollitia animi culpa eaque voluptate quo nostrum qui accusamus doloribus repudiandae alias sed id maxime. Alias, ab obcaecati.
+            </p>
+        </div>
+  </div>
+  )
+};
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import HeroImage from '../assets/image.jpg';
+import { RxDoubleArrowRight } from "react-icons/rx";
+
+
+const Home = () => {
+  return (
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+        <div className='flex flex-col justify-center h-full'>
+          <h2 className='text-4xl sm:text-7xl font-bold text-white'>
+            I'm a Full Stack Developer
+          </h2>
+          <p className='text-gray-500 py-4 max-w-md'>
+            Self-motivated individual seeking a position where I can apply my technical expertise and leverage my
+            excellent academic background to foster personal growth and gain valuable experience.
+          </p>
+          <div>
+            <button to='portfolio' smooth duration={500} className='group text-white px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-700 cursor-pointer'>
+              Portfolio <span className='group-hover:rotate-90 duration-300'><RxDoubleArrowRight size={20} className='ml-1' /></span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <img src={HeroImage} alt='my-profile' className='rounded-2xl mx-auto w-2/3 md:w-full' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
